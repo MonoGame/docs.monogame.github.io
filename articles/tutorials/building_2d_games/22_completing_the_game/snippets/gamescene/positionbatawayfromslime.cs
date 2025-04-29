08e8e38618483a584c87ebc6233cad7cd3b3bcb8
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2025-04-28 22:14:49 -0400

    Resolve bug in position away from slime code

--- articles/tutorials/building_2d_games/22_completing_the_game/snippets/gamescene/positionbatawayfromslime.cs
+++ articles/tutorials/building_2d_games/22_completing_the_game/snippets/gamescene/positionbatawayfromslime.cs
@@ -1,61 +1,73 @@
 private void PositionBatAwayFromSlime()
 {
-    // Get the bounds of the slime and the bat.
+    // Calculate the position that is in the center of the bounds
+    // of the room.
+    float roomCenterX = _roomBounds.X + _roomBounds.Width * 0.5f;
+    float roomCenterY = _roomBounds.Y + _roomBounds.Height * 0.5f;
+    Vector2 roomCenter = new Vector2(roomCenterX, roomCenterY);
+
+    // Get the bounds of the slime and calculate the center position
     Circle slimeBounds = _slime.GetBounds();
-    Circle batBounds = _bat.GetBounds();
+    Vector2 slimeCenter = new Vector2(slimeBounds.X, slimeBounds.Y);
 
-    // Get the center of the room bounds
-    float centerX = _roomBounds.X + _roomBounds.Width * 0.5f;
-    float centerY = _roomBounds.Y + _roomBounds.Height * 0.5f;
-    Vector2 center = new Vector2(centerX, centerY);
+    // Calculate the distance vector from the center of the room to the
+    // center of the slime.
+    Vector2 centerToSlime = slimeCenter - roomCenter;
 
-    // Get the center of the slime bounds
-    Vector2 slimePos = new Vector2(slimeBounds.X, slimeBounds.Y);
+    // Get the bounds of the bat
+    Circle batBounds =_bat.GetBounds();
 
-    // Calculate the vector from the center of the room to the slime.
-    Vector2 centerToSlime = slimePos - center;
+    // Calculate the amount of padding we will add to the new position of
+    // the bat to ensure it is not sticking to walls
+    int padding = batBounds.Radius * 2;
 
-    // Get the current position of the bat so we can adjust it.
-    Vector2 newBatPosition = _bat.Position;
-
-    // Determine the furthest wall by finding which component (x or y) is
-    // larger and in which direction
+    // Calculate the new position of the bat by finding which component of
+    // the center to slime vector (X or Y) is larger and in which direction.
+    Vector2 newBatPosition = Vector2.Zero;
     if (Math.Abs(centerToSlime.X) > Math.Abs(centerToSlime.Y))
     {
-        // Slime is closer to either the left or right wall, so the Y position
-        // will be the center Y position
-        newBatPosition.Y = center.Y;
+        // The slime is closer to either the left or right wall, so the Y
+        // position will be a random position between the top and bottom
+        // walls.
+        newBatPosition.Y = Random.Shared.Next(
+            _roomBounds.Top + padding,
+            _roomBounds.Bottom - padding
+        );
 
-        // Now determine the X position based on which wall the slime is
-        // closest to.
         if (centerToSlime.X > 0)
         {
-            // Slime is on the right side, place the bat on the left wall
-            newBatPosition.X = _roomBounds.Left;
+            // The slime is closer to the right side wall, so place the
+            // bat on the left side wall
+            newBatPosition.X = _roomBounds.Left + padding;
         }
         else
         {
-            // Slime is on the left side, place the bat on the right wall
-            newBatPosition.X = _roomBounds.Right - batBounds.Radius * 2.0f;
+            // The slime is closer ot the left side wall, so place the
+            // bat on the right side wall.
+            newBatPosition.X = _roomBounds.Right - padding * 2;
         }
     }
     else
     {
-        // Slime is closer to either the top or bottom wall, so the X position
-        // will be the center X position
-        newBatPosition.X = center.X;
+        // The slime is closer to either the top or bottom wall, so the X
+        // position will be a random position between the left and right
+        // walls.
+        newBatPosition.X = Random.Shared.Next(
+            _roomBounds.Left + padding,
+            _roomBounds.Right - padding
+        );
 
-        // Now determine the Y position based on which wall the slime is
-        // closest to.
         if (centerToSlime.Y > 0)
         {
-            // Slime is closer to the bottom, place the bat on the top wall.
-            newBatPosition.Y = _roomBounds.Top;
+            // The slime is closer to the top wall, so place the bat on the
+            // bottom wall
+            newBatPosition.Y = _roomBounds.Top + padding;
         }
         else
         {
-            // Slime is closer to the top, place the bat on the bottom wall.
-            newBatPosition.Y = _roomBounds.Bottom - batBounds.Radius * 2.0f;
+            // The slime is closer to the bottom wall, so place the bat on
+            // the top wall.
+            newBatPosition.Y = _roomBounds.Bottom - padding * 2;
         }
     }
 
