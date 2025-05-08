38c0b104f30c18b2a207b186530896d49de004b9
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2025-05-08 17:06:41 -0400

    Remove using statement. Dock enum needs to be explicit

--- articles/tutorials/building_2d_games/21_customizing_gum_ui/snippets/animatedbutton.cs
+++ articles/tutorials/building_2d_games/21_customizing_gum_ui/snippets/animatedbutton.cs
@@ -3,7 +3,6 @@ using Gum.DataTypes;
 using Gum.DataTypes.Variables;
 using Gum.Graphics.Animation;
 using Gum.Managers;
-using Gum.Wireframe;
 using Microsoft.Xna.Framework.Input;
 using MonoGameGum.Forms.Controls;
 using MonoGameGum.GueDeriving;
@@ -37,7 +36,7 @@ internal class AnimatedButton : Button
         nineSliceInstance.Height = 0f;
         nineSliceInstance.Texture = atlas.Texture;
         nineSliceInstance.TextureAddress = TextureAddress.Custom;
-        nineSliceInstance.Dock(Dock.Fill);
+        nineSliceInstance.Dock(Gum.Wireframe.Dock.Fill);
         topLevelContainer.Children.Add(nineSliceInstance);
 
         // Create the text element that will display the button's label
@@ -51,7 +50,7 @@ internal class AnimatedButton : Button
         textInstance.UseCustomFont = true;
         textInstance.CustomFontFile = "fonts/04b_30.fnt";
         textInstance.FontScale = 0.25f;
-        textInstance.Anchor(Anchor.Center);
+        textInstance.Anchor(Gum.Wireframe.Anchor.Center);
         textInstance.Width = 0;
         textInstance.WidthUnits = DimensionUnitType.RelativeToChildren;
         topLevelContainer.Children.Add(textInstance);
