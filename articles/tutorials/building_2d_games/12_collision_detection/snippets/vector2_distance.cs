11d68143c1ade390d797bd19d9fe22b02e807f56
Mike <MikeA1@users.noreply.github.com>
2025-06-09 17:59:59 -0600

    Switch circle radii back to 5

--- articles/tutorials/building_2d_games/12_collision_detection/snippets/vector2_distance.cs
+++ articles/tutorials/building_2d_games/12_collision_detection/snippets/vector2_distance.cs
@@ -1,8 +1,8 @@
 Vector2 circle1Position = new Vector2(8, 10);
 Vector2 circle2Position = new Vector2(5, 6);
 
-float circle1Radius = 3;
-float circle2Radius = 3;
+float circle1Radius = 5;
+float circle2Radius = 5;
 
 // c^2 = a^2 + b^2
 // c^2 = (8 - 5)^2 + (10 - 6)^2
@@ -11,12 +11,12 @@ float circle2Radius = 3;
 // c^2 = 25
 float distanceSquared = Vector2.DistanceSquared(circle1Position, circle2Position);
 
-// r^2 = (3 + 3)^2
-// r^2 = (6)^2
-// r^2 = 36
+// r^2 = (5 + 5)^2
+// r^2 = (10)^2
+// r^2 = 100
 int radiiSquared = (circle1Radius + circle2Radius) * (circle1Radius + circle2Radius);
 
-// The circles overlap because 36 is greater than 25
+// The circles overlap because 100 is greater than 25
 if(radiiSquared > distanceSquared)
 {
     
